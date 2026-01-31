466a3c470b25f59548e1fb735b2511503e78301e
G.C.M. <charithmanujaya1@gmail.com>
2026-02-01 03:19:29 +0530

    new

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,5 @@
 
 use App\Http\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Route;
-use Filament\Facades\Filament;
 
 Route::get('invoices/{id}/pdf', [InvoiceController::class, 'generateInvoice'])->name('invoices.pdf');
-
-Route::get('/admin', function () {
-    $panel = Filament::getPanel('admin');
-    return $panel->getPages()[0]::make()->render();  // force render dashboard
-});
-
-Route::get('/test-alive', function () {
-    return 'Laravel is alive! Current time: ' . now()->toDateTimeString();
-});
